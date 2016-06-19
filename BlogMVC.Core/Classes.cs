4ad070f9d6d97f334149eed9178dc0cf58383daa
HilalSener <crescent.forest@hotmail.com>
2016-06-19 18:47:53 +0300

    Css template copied into MVC project and added databases.

---
+++ BlogMVC.Core/Classes.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlogMVC.Core
+{
+        public interface IBlogRepository
+        {
+            IList<Post> Posts(int pageNo, int pageSize);
+            int TotalPosts();
+        }
+        public class Post
+        {
+            public virtual int Id
+            { get; set; }
+
+            public virtual string Title
+            { get; set; }
+
+            public virtual string ShortDescription
+            { get; set; }
+
+            public virtual string Description
+            { get; set; }
+
+            public virtual string Meta
+            { get; set; }
+
+            public virtual string UrlSlug
+            { get; set; }
+
+            public virtual bool Published
+            { get; set; }
+
+            public virtual DateTime PostedOn
+            { get; set; }
+
+            public virtual DateTime? Modified
+            { get; set; }
+
+            public virtual Category Category
+            { get; set; }
+
+            public virtual IList<Tag> Tags
+            { get; set; }
+        }
+
+        public class Category
+        {
+            public virtual int Id
+            { get; set; }
+
+            public virtual string Name
+            { get; set; }
+
+            public virtual string UrlSlug
+            { get; set; }
+
+            public virtual string Description
+            { get; set; }
+
+            public virtual IList<Post> Posts
+            { get; set; }
+        }
+
+        public class Tag
+        {
+            public virtual int Id
+            { get; set; }
+
+            public virtual string Name
+            { get; set; }
+
+            public virtual string UrlSlug
+            { get; set; }
+
+            public virtual string Description
+            { get; set; }
+
+            public virtual IList<Post> Posts
+            { get; set; }
+        }
+        
+
+}
---
+++ BlogMVC.DataModel/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using BlogMVC.Core;
+
+namespace BlogMVC.DataModel
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+   
+    }
+}
